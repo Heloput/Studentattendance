6d6f64282d24561285684efcec3d3bf9580f1a10
Владислав Молодожонов <molodozhonovladik@mail.ru>
2022-05-25 14:08:19 +0400

    Update add window and add counter rows

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-import string
 import csv
 
 reader = 0
@@ -11,6 +10,8 @@ admin_login = "admin"
 admin_password = "1234"
 user_login = "guest"
 user_password = "111"
+PID = 1
+IID = 1
 
 
 def download_window():
@@ -30,7 +31,8 @@ def download_window():
         window.geometry('300x100+700+400')
         window.resizable(False, False)
         print("Файл не существует!")
-        download_lbl = tk.Label(window, text="Отсутствует файл!\n Проверьте его на наличие!\nЗагрузите файл из архива.", fg="#eee", bg="gray22", justify="center")
+        download_lbl = tk.Label(window, text="Отсутствует файл!\n Проверьте его на наличие!\nЗагрузите файл из архива.",
+                                fg="#eee", bg="gray22", justify="center")
         download_lbl.place(relx=.2, rely=.2)
         button = tk.Button(window, text="Окей", command=window.destroy)
         button.place(relx=.85, rely=.7)
@@ -69,13 +71,13 @@ def authorization_window():
     window["bg"] = "gray22"
     window.geometry('200x150+700+400')
     window.attributes('-toolwindow', True)
-    main_label = tk.Label(window, text='Авторизация', justify="center",  fg="#eee", bg="gray22")
+    main_label = tk.Label(window, text='Авторизация', justify="center", fg="#eee", bg="gray22")
     main_label.pack()
-    user_label = tk.Label(window, text='Имя пользователя', justify="center",  fg="#eee", bg="gray22")
+    user_label = tk.Label(window, text='Имя пользователя', justify="center", fg="#eee", bg="gray22")
     user_label.pack()
     username_entry = tk.Entry(window, bg='gray', fg='#000')
     username_entry.pack()
-    password_label = tk.Label(window, text='Пароль', justify="center",  fg="#eee", bg="gray22")
+    password_label = tk.Label(window, text='Пароль', justify="center", fg="#eee", bg="gray22")
     password_label.pack()
     password_entry = tk.Entry(window, bg='gray', fg='#000', show="*")
     password_entry.pack()
@@ -86,17 +88,18 @@ def authorization_window():
 
 
 def main_window():
+    global PID
+    global IID
     window = tk.Tk()
-    window.title
     window.title("Учёт посещаемости студентов")
     window["bg"] = "gray22"
     window.geometry('800x450+200+200')
     window.resizable(0, 0)  # делает неактивной кнопку Развернуть
     frame_list = tk.Frame(window, bg='gray')
-    frame_list.grid(column=0, row=0, sticky='nsew')
+    frame_list.grid(column=0, row=0, sticky='we')
 
     table = ttk.Treeview(frame_list)
-    table['columns'] = [0, 1, 2, 3, 4, 5, 6, 7]
+    table['columns'] = [0, 1, 2, 3, 4, 5, 6]
     table.heading('#0', text='№')
     table.heading("#1", text="Фамилия")
     table.heading("#2", text="Имя")
@@ -105,45 +108,91 @@ def main_window():
     table.heading("#5", text="Статус")
     table.heading("#6", text="Предмет")
     table.heading("#7", text="Пара")
-    table.heading("#8", text="Преподаватель")
-    table.column("#0", width=30, anchor='e')
+    table.column("#0", width=40, anchor='e')
     table.column("#1", width=100)
     table.column("#2", width=100)
     table.column("#3", width=100)
-    table.column("#4", width=50)
+    table.column("#4", width=70)
     table.column("#5", width=50, anchor="center")
     table.column("#6", width=100, anchor="center")
     table.column("#7", width=50, anchor="center")
-    table.column("#8", width=100, anchor="center")
-
-    id = 0
-    iid = 0
 
     for row in reader:
-        table.insert('', tk.END, text=str(id), values=row)
-        iid += 1
-        id += 1
+        table.insert('', tk.END, iid=IID, text=str(PID), values=row)
+        IID += 1
+        PID += 1
 
-    scroll_pane = ttk.Scrollbar(frame_list, command=table.yview())
-    table.configure(yscrollcommand=scroll_pane.set)
+    scroll_pane = ttk.Scrollbar(frame_list, orient=tk.VERTICAL, command=table.yview)
+    table.configure(yscroll=scroll_pane.set)
     scroll_pane.pack(side=tk.RIGHT, fill=tk.Y)
     table.pack(expand=tk.YES, fill=tk.BOTH)
 
-    def insert_data():
-        table.insert('', tk.END, iid=iid)
+    def insert_form():
+        global PID
+        global IID
+        add_window = tk.Tk()
+        add_window.title("Ввод значений")
+        add_window["bg"] = "gray22"
+        add_window.geometry('300x250+900+300')
+        add_window.attributes('-toolwindow', True)
+        family_label = tk.Label(add_window, text='Фамилия', fg="#eee", bg="gray22")
+        family_label.grid(row=0, column=0)
+        family_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        family_entry.grid(row=0, column=1)
+        user_label = tk.Label(add_window, text='Имя', justify="center", fg="#eee", bg="gray22")
+        user_label.grid(row=1, column=0)
+        user_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        user_entry.grid(row=1, column=1)
+        last_label = tk.Label(add_window, text='Отчество', justify="center", fg="#eee", bg="gray22")
+        last_label.grid(row=2, column=0)
+        last_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        last_entry.grid(row=2, column=1)
+        date_label = tk.Label(add_window, text='Дата', justify="center", fg="#eee", bg="gray22", width=20)
+        date_label.grid(row=3, column=0)
+        date_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        date_entry.grid(row=3, column=1)
+        status_label = tk.Label(add_window, text='Статус', justify="center", fg="#eee", bg="gray22", width=20)
+        status_label.grid(row=4, column=0)
+        status_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        status_entry.grid(row=4, column=1)
+        subject_label = tk.Label(add_window, text='Предмет', justify="center", fg="#eee", bg="gray22", width=20)
+        subject_label.grid(row=5, column=0)
+        subject_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        subject_entry.grid(row=5, column=1)
+        schedule_label = tk.Label(add_window, text='Пара', justify="center", fg="#eee", bg="gray22", width=20)
+        schedule_label.grid(row=6, column=0)
+        schedule_entry = tk.Entry(add_window, bg='gray', fg='#000')
+        schedule_entry.grid(row=6, column=1)
 
-    insert_button = tk.Button(window, text="Добавить", command=insert_data)
-    insert_button.grid(row=2, column=5)
+        def insert_data():
+            global PID
+            global IID
+            if len(family_entry.get()) == 0 and len(user_entry.get()) == 0 and len(last_entry.get()) == 0 and len(date_entry.get()) == 0 and len(subject_entry.get()) == 0:
+                messagebox.showwarning("Вводимые поля", "Пустые поля!\nВведите данные!")
+            else:
+                table.insert('', tk.END, iid=IID, text=str(PID), values=(
+                    family_entry.get(), user_entry.get(), last_entry.get(), date_entry.get(), status_entry.get(),
+                    subject_entry.get(), schedule_entry.get()))
+                add_window.destroy()
+                PID += 1
+                IID += 1
+
+        add_btn = tk.Button(add_window, text="Добавить", command=insert_data)
+        add_btn.grid(row=7, column=0, columnspan=2)
+
+        add_window.update()
+        add_window.mainloop()
+
+    insert_button = tk.Button(window, text="Добавить", command=insert_form, justify="center")
+    insert_button.grid(row=0, column=2)
 
     window.update()
     window.mainloop()
 
 
-
-
-
-
 if download_window():
-    #authorization_window()
-    #if isAdmin == 1 or isAdmin == 2:
+    # authorization_window()
+
+    # if isAdmin == 1 or isAdmin == 2:
+
     main_window()
